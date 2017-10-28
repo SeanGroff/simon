9ef286af74e9f1424eac4c85fc329dbe2ba92fd8
Sean <sean@codekoalas.com>
2017-10-28 18:29:23 -0500

    Lights ducks configured & lights will light up after thunk is built 2

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
-// import lights from './lights';
+import lights from './lights';
 // import match from './match';
 import game from './game';
 
 export default combineReducers({
-  // lights,
+  lights,
   game,
   // match,
 });
