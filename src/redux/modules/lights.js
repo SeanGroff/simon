eabcf2f8e05263c9ed66085022a9dc3daceb49d4
Sean <sean@codekoalas.com>
2017-11-29 00:43:02 -0600

    added memoized selectors to lights container

--- src/redux/modules/lights.js
+++ src/redux/modules/lights.js
@@ -1,3 +1,5 @@
+import { createSelector } from 'reselect';
+
 import { TURN_LIGHT_ON, TURN_LIGHT_OFF, TOGGLE_GAME_POWER } from './game';
 
 type LightsState = [
@@ -6,6 +8,43 @@ type LightsState = [
     +active: boolean,
   },
 ];
+
+/**
+ * Selectors
+ */
+const counterSelector = state => state.game.counter;
+const lightsSelector = state => state.lights;
+const audioPlayingSelector = state => state.game.audioPlaying;
+const powerSelector = state => state.game.power;
+const gameOverSelector = state => state.game.gameOver;
+
+/**
+ * Get Active Light Selector
+ */
+export const getActiveLightSelector = createSelector(
+  [counterSelector, lightsSelector],
+  (counter, lights) => {
+    if (counter) {
+      return lights.reduce((accum, light) => {
+        if (light.active) {
+          return light.color;
+        }
+        return accum;
+      }, '');
+    }
+  },
+);
+
+/**
+ * Is Light clickable selector
+ */
+export const isLightClickableSelector = createSelector(
+  [audioPlayingSelector, powerSelector, gameOverSelector, counterSelector],
+  (audioPlaying, power, gameOver, counter) =>
+    !audioPlaying && power && !gameOver && counter !== 'X' && !!counter
+      ? true
+      : false,
+);
 
 const initialState = [
   {
